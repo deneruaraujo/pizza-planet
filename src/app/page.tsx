e8be98ced5c255174a19fd34e70d3a837a64656d
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2024-03-13 12:18:56 -0300

    Add: Dynamic rendering of components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import { ProductCard } from "../components/product/ProductCard";
+import storeItems from '@/server.json'
 
 export default function Home() {
   return (
     <main>
       <section className="mx-10 my-10 flex flex-wrap justify-center gap-20">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {storeItems.products.map(item => (
+          <ProductCard key={item.id} {...item} />
+        ))}
       </section>
     </main>
   );
--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -1,35 +1,36 @@
+import { CurrencyFormat } from "@/src/utils/currencyFormat";
 import Image from "next/image";
-import pizzaPortuguesa from "@/public/product/pizza-portuguesa.jpg"
 import { PiMinus, PiPlus, PiShoppingCart } from "react-icons/pi";
 
-export function ProductCard() {
+export interface ProductCardProps {
+  name: string,
+  src: string,
+  price: number,
+}
+
+export function ProductCard({ name, src, price }: ProductCardProps) {
   return (
-    <div className="shadow-inner shadow-gray-500 bg-gradient-to-l from-gray-300 via-white to-gray-200 rounded-t-3xl rounded-b-lg flex flex-col items-center">
-      <div>
-        <Image
-          src={pizzaPortuguesa}
-          width={250}
-          height={0}
-          quality={100}
-          alt=""
-          className="rounded-t-3xl"
-        />
-      </div>
-      <div>
+    <div className="shadow-inner shadow-gray-500 bg-gradient-to-l from-gray-300 via-white to-gray-200 rounded-t-3xl rounded-b-lg flex flex-col items-center w-60">
+      <Image
+        src={src}
+        width={250}
+        height={0}
+        quality={100}
+        alt=""
+        className="rounded-t-3xl h-40"
+      />
+      <div className="flex flex-col items-center justify-end h-full">
         <div className="py-4 font-bold flex flex-col items-center">
-          <span>Pizza Portuguesa</span>
-          <span>R$ 9,90</span>
+          <span className="text-center text-wrap whitespace-normal break-words">{name}</span>
+          <span>{CurrencyFormat(price)}</span>
         </div>
-        <div className="flex gap-4 mr-2 py-2 px-2 rounded-t-md rounded-rb-sm rounded-lb-sm items-center justify-center shadow-inner shadow-gray-400 hover:text-black hover:shadow-gray-900 duration-200">
+        <div className="flex gap-4 py-2 px-6 rounded-t-md rounded-rb-sm rounded-lb-sm items-center justify-center shadow-inner shadow-gray-400 hover:text-black hover:shadow-gray-900 duration-200">
           <button className="hover:text-red-600 px-1">
-            <PiMinus size={15} aria-label="remover quantidade" title="remover quantidade" />
+            <PiMinus size={15} aria-label="remover 1" title="remover 1" />
           </button>
           <span aria-label="quantidade">0</span>
           <button className="hover:text-red-600 px-1">
-            <PiPlus size={15} aria-label="adicionar quantidade" title="adicionar quantidade" />
-          </button>
-          <button className="hover:text-red-600 px-1">
-            <PiShoppingCart size={22} aria-label="mover para o carrinho" title="mover para o carrinho" />
+            <PiPlus size={15} aria-label="adicionar 1" title="adicionar 1" />
           </button>
         </div>
       </div>
